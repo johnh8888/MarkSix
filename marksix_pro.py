129b32f44cbcf91b0b6ffb05c870044d3158d4f4
johnh8888 <johnhao88889@gmail.com>
2026-04-14 22:55:57 +0800

    Create marksix_pro.py

---
+++ marksix_pro.py
@@ -0,0 +1,4 @@
+python marksix_pro.py sync \
+  --third-party-url https://lottolyzer.com/history/hong-kong/mark-six/page/1/per-page/50/summary-view \
+  --third-party-url https://marksix6.net/index.php?api=1 \
+  --third-party-max-pages 10
